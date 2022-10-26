6abdfe4deee67bf143582e1a707042eb3acd63fd
RAVI SHEKHAR TIWARI <tiwari11.rst@gmail.com>
2022-10-26 15:38:07 +0530

    done

--- django_api_ml/django_api_ml/settings.py
+++ django_api_ml/django_api_ml/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
      "http://127.0.0.1:8052",
      "http://127.0.0.1:8051",
